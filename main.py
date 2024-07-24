6b1bedee3cd4685926cd66d9fcb167e0b2bdfec3
Олег <113263638+OLEJOJEK23@users.noreply.github.com>
2024-07-24 18:53:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,20 +13,7 @@ cookies = {
 }
 
 headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-    'Connection': 'keep-alive',
-    'If-Modified-Since': 'Sat, 13 Apr 2024 06:26:09 GMT',
-    'Referer': 'https://auto.drom.ru/all/page2/',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'same-origin',
-    'Sec-Fetch-User': '?1',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-    'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
+    #headers
 }
 pages = 2
 marks = {}
